d12d1ec4d60d94dd38ac5c340fb9a297f2142e51
Gus-Victrix <iamfrsncis@gmail.com>
2023-11-01 02:25:04 +0300

    Cleaned up task 9
    Betty linting and syntax related.

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,20 +1,22 @@
 #include "binary_trees.h"
 
 /**
-  * binary_tree_height - Measures the height of a binary tree.
-  *
-  * tree: The tree whose depth is to be measured.
-  */
+ * binary_tree_height - Measures the height of a binary tree.
+ *
+ * @tree: The tree whose depth is to be measured.
+ *
+ * Return: The maximum height of the binary tree.
+ */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-    size_t left = 0, right = 0;
+	size_t left = 0, right = 0;
 
-    if (!tree)
-      return (0);
-    left = binary_tree_height(tree->left);
-    right = binary_tree_height(tree->right);
+	if (!tree)
+		return (0);
+	left = binary_tree_height(tree->left);
+	right = binary_tree_height(tree->right);
 
-    if (left >= right)
-      return (left + 1);
-    return (right + 1);
+	if (left >= right)
+		return (left + 1);
+	return (right + 1);
 }

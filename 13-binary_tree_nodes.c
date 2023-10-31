cf70e334d26e51e681b7b583d0c0bcb4cfb4fa4e
Francis Njoroge Nderitu <117985602+Gus-Victrix@users.noreply.github.com>
2023-10-31 13:16:50 +0300

    Create 13-binary_tree_nodes.c
    Added task 13.

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+
+/**
+  * binary_tree_nodes - Counts the number of nodes with at least one child in a binary tree.
+  *
+  * tree: The tree whose nodes are to be counted.
+  *
+  * Return: The number of nodes having children.
+  */
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+  size_t count = 0;
+
+  if (!tree)
+    return (0);
+  if (tree->left || tree->right)
+    count = 1;
+  return (count + binary_tree_nodes(tree->left) + binary_tree_nodes->right);
+}

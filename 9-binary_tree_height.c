eeeaaee7af104286a03ec1a88ddba685ce2094f0
Francis Njoroge Nderitu <117985602+Gus-Victrix@users.noreply.github.com>
2023-10-31 12:25:27 +0300

    Create 9-binary_tree_height.c
    Added task 9.

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+
+/**
+  * binary_tree_height - Measures the height of a binary tree.
+  *
+  * tree: The tree whose depth is to be measured.
+  */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+    size_t left = 0, right = 0;
+
+    if (!tree)
+      return (0);
+    left = binary_tree_height(tree->left);
+    right = binary_tree_height(tree->right);
+
+    if (left >= right)
+      return (left + 1);
+    return (right + 1);
+}

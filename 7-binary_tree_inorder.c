e8253ad8a037a7dd1252dcd8e2215576d76603a4
Gus-Victrix <iamfrsncis@gmail.com>
2023-11-01 02:14:47 +0300

    Cleaned up task 7
    Betty linter and syntax related.

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -1,16 +1,16 @@
 #include "binary_trees.h"
 
 /**
-  * binary_tree_inorder - Traverses a binary_tree_t via an in-order approach.
-  *
-  * tree: The tree to be traversed.
-  * func: The function to be performed on the nodes of the tree.
-  */
+ * binary_tree_inorder - Traverses a binary_tree_t via an in-order approach.
+ *
+ * @tree: The tree to be traversed.
+ * @func: The function to be performed on the nodes of the tree.
+ */
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 {
-    if (!tree || !func)
-        return;
-    binary_tree_inorder(tree->left);
-    func(tree->n);
-    binary_tree_inorder(tree->right);
+	if (!tree || !func)
+		return;
+	binary_tree_inorder(tree->left, func);
+	func(tree->n);
+	binary_tree_inorder(tree->right, func);
 }

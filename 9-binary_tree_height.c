464c6a3e0a8bdd83a086701f222ef44651be5879
Gus-Victrix <iamfrsncis@gmail.com>
2023-11-01 02:45:37 +0300

    Logic cleanup for task 9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,7 +1,28 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_height - Measures the height of a binary tree.
+ * binary_tree_height_recursion - Measures the height of a binary tree recurse.
+ *
+ * @tree: The tree whose depth is to be measured.
+ *
+ * Return: The maximum height of the binary tree.
+ */
+size_t binary_tree_height_recursion(const binary_tree_t *tree)
+{
+	size_t left = 0, right = 0;
+
+	if (!tree)
+		return (0);
+	left += binary_tree_height_recursion(tree->left);
+	right += binary_tree_height_recursion(tree->right);
+
+	if (left >= right)
+		return (left + 1);
+	return (right + 1);
+}
+
+/**
+ * binary_tree_height - Outer wrapper for the binary tree height function.
  *
  * @tree: The tree whose depth is to be measured.
  *
@@ -13,10 +34,10 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 	if (!tree)
 		return (0);
-	left = binary_tree_height(tree->left);
-	right = binary_tree_height(tree->right);
+	left += binary_tree_height_recursion(tree->left);
+	right += binary_tree_height_recursion(tree->right);
 
 	if (left >= right)
-		return (left + 1);
-	return (right + 1);
+		return (left);
+	return (right);
 }

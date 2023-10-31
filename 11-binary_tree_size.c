09591658e5a54868e61c7e150748cee5db7295e4
Gus-Victrix <iamfrsncis@gmail.com>
2023-11-01 02:48:17 +0300

    Cleaned up task 11
    Linting errors

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -1,15 +1,15 @@
 #include "binary_trees.h"
 
 /**
-  * binary_tree_size - Counts the number of nodes in a binary_tree_t tree.
-  *
-  * tree: The tree whose nodes are to be counted.
-  *
-  * Return: Number of nodes in the tree. 0 if tree is null.
-  */
+ * binary_tree_size - Counts the number of nodes in a binary_tree_t tree.
+ *
+ * @tree: The tree whose nodes are to be counted.
+ *
+ * Return: Number of nodes in the tree. 0 if tree is null.
+ */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-    if (!tree)
-        return (0);
-    return (binary_tree_size(tree->left) + binary_tree_size(tree->right) + 1);
+	if (!tree)
+		return (0);
+	return (binary_tree_size(tree->left) + binary_tree_size(tree->right) + 1);
 }

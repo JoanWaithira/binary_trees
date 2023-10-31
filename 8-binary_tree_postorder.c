6f8c2104e285c55091b4ce20e16f5d90efd84ab0
Francis Njoroge Nderitu <117985602+Gus-Victrix@users.noreply.github.com>
2023-10-31 12:00:59 +0300

    Create 8-binary_tree_postorder.c
    Added task 8

---
+++ 8-binary_tree_postorder.c
@@ -0,0 +1,16 @@
+#include "bianary_trees.h"
+
+/**
+  * binary_tree_postorder - Traverses a binary_tree_t via a post-order approach.
+  *
+  * tree: The tree to be traversed.
+  * func: The function to be performed on the data in the tree's nodes.
+  */
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
+{
+    if (!tree || !func)
+        return;
+    binary_tree_postorder(tree->left);
+    binary_tree_postorder(tree->right);
+    func(tree->n);
+}

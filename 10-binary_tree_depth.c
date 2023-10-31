15a9f75b989375c650792eca053799aba9d1172c
Francis Njoroge Nderitu <117985602+Gus-Victrix@users.noreply.github.com>
2023-10-31 12:36:53 +0300

    Create 10-binary_tree_depth.c
    Added task 10.

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,25 @@
+#include "bianary_trees.h"
+
+/**
+  * bianary_tree_depth - Measures the depth of a binary_tree_t node from the root.
+  *
+  * tree: The node whose depth is to be measured.
+  *
+  * Returns: The depth of the node.
+  */
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+    binary_tree_t node;
+    size_t depth = 0;
+
+    if (!tree)
+      return (0);
+
+    node = tree;
+    while (node)
+    {
+        node = node->parent;
+        depth++;
+    }
+    return (depth);
+}

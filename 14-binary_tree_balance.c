d309502a36d1ab41b55a857a42d89dd9312f3b27
Gus-Victrix <iamfrsncis@gmail.com>
2023-11-01 03:36:46 +0300

    Cleaned up task 14
    Linting related and syntax

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,34 +1,40 @@
 #include "binary_trees.h"
 
 /**
-  * binary_tree_height_ - Measures the height of a binary tree.
-  *
-  * tree: The tree whose height is to be measured.
-  */
+ * binary_tree_height_ - Measures the height of a binary tree.
+ *
+ * @tree: The tree whose height is to be measured.
+ *
+ * Return: Max height of left branch - max height of right branch.
+ */
 size_t binary_tree_height_(const binary_tree_t *tree)
 {
-    size_t left = 0, right = 0;
+	size_t left = 0, right = 0;
 
-    if (!tree)
-      return (0);
-    left = binary_tree_height(tree->left);
-    right = binary_tree_height(tree->right);
+	if (!tree)
+		return (0);
+	left = binary_tree_height_(tree->left);
+	right = binary_tree_height_(tree->right);
 
-    if (left >= right)
-      return (left + 1);
-    return (right + 1);
+	if (left >= right)
+		return (left + 1);
+	return (right + 1);
 }
 
 /**
-  * binary_tree_balance - Calculates the balance factor of the input binary tree.
+  * binary_tree_balance - Calculates the balance factor of binary tree.
   *
-  * tree: The tree whose balance factor is to be calculated.
+  * @tree: The tree whose balance factor is to be calculated.
   *
-  * Return: The difference between the height of the left branch and the right one.
+  * Return: The diff between the height of the left branch and the right one.
   */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-  if (!tree)
-    return (0);
-  return ((int)(binary_tree_height_(tree->left) - binary_tree_height_(tree->right)));
+	size_t left, right;
+
+	if (!tree)
+		return (0);
+	left = binary_tree_height_(tree->left);
+	right = binary_tree_height_(tree->right);
+	return ((int)(left - right));
 }

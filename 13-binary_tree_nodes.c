babca90147767289a7136dec74291050da752bee
Gus-Victrix <iamfrsncis@gmail.com>
2023-11-01 03:23:44 +0300

    Cleaned up task 13

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -1,19 +1,21 @@
 #include "binary_trees.h"
 
 /**
-  * binary_tree_nodes - Counts the number of nodes with at least one child in a binary tree.
-  *
-  * tree: The tree whose nodes are to be counted.
-  *
-  * Return: The number of nodes having children.
-  */
+ * binary_tree_nodes - Counts the number of nodes with at least one child.
+ *
+ * @tree: The tree whose nodes are to be counted.
+ *
+ * Return: The number of nodes having children.
+ */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-  size_t count = 0;
+	size_t count = 0, left, right;
 
-  if (!tree)
-    return (0);
-  if (tree->left || tree->right)
-    count = 1;
-  return (count + binary_tree_nodes(tree->left) + binary_tree_nodes->right);
+	if (!tree)
+		return (0);
+	if (tree->left || tree->right)
+		count = 1;
+	left = binary_tree_nodes(tree->left);
+	right = binary_tree_nodes(tree->right);
+	return (count + left + right);
 }

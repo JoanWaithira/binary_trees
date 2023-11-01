3a94d5a45602e610c728e348e2d5d22388314658
Gus-Victrix <iamfrsncis@gmail.com>
2023-11-01 21:08:17 +0300

    Rectifications to task 12

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -1,37 +1,17 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_leaves_recursion - Counts the leaves in a binary tree.
+ * binary_tree_leaves - Count the number of leaves of a binary tree.
  *
- * @tree: The tree whose leaves are to be counted.
+ * @tree: The tree whose nodes are to be counted.
  *
- * Return: Number of leaves in the tree.
- */
-size_t binary_tree_leaves_recursion(const binary_tree_t *tree)
-{
-	size_t left, right;
-
-	if (!tree)
-		return (0);
-	right = binary_tree_leaves_recursion(tree->right);
-	left = binary_tree_leaves_recursion(tree->left);
-	return (1 + left + right);
-}
-
-/**
- * binary_tree_leaves - Wrapper for the recursive function.
- *
- * @tree: The tree whose leaves are to be counted.
- *
- * Return: Number of leaves in the tree.
+ * Return: The number of leaves or 0 if null.
  */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-	size_t left, right;
-
 	if (!tree)
 		return (0);
-	right = binary_tree_leaves_recursion(tree->right);
-	left = binary_tree_leaves_recursion(tree->left);
-	return (left + right);
+	if (!tree->left && !tree->right)
+		return (1);
+	return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
 }

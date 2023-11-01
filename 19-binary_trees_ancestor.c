011163854c1e1ea4e695bf590a45612125e6ab89
Gus-Victrix <iamfrsncis@gmail.com>
2023-11-01 23:13:31 +0300

    Added task 19

---
+++ 19-binary_trees_ancestor.c
@@ -0,0 +1,25 @@
+#include "binary_trees.h"
+
+/**
+ * binary_trees_ancestor - Checks the first common ancestor node of two.
+ *
+ * @first: One of the nodes being checked.
+ * @second: One of the nodes being checked.
+ *
+ * Return: The lowest common ancestor node of the two input nodes.
+ */
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const
+		binary_tree_t *second)
+{
+	binary_tree_t *one, *two;
+
+	if (!first || !second)
+		return (NULL);
+	if (first->parent == second->parent)
+		return (first->parent);
+	one = binary_trees_ancestor(first->parent, second);
+	two = binary_trees_ancestor(first, second->parent);
+	if (!two)
+		return (one);
+	return (two);
+}

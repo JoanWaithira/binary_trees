92ea9cf993079d50abd02b23c9bfd1373a4ca0f7
Gus-Victrix <iamfrsncis@gmail.com>
2023-11-01 22:46:20 +0300

    Modified the returns for perfect vs not perfect.

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -53,7 +53,7 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 {
 	if (!tree)
 		return (0);
-	if (binary_tree_max_height(tree) == binary_tree_min_height(tree))
+	if (binary_tree_max_height(tree) != binary_tree_min_height(tree))
 		return (0);
 	return (1);
 }

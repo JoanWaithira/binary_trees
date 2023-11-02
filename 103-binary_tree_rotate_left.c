1a7b49c8f54fdf46f5c4155ea315306089154db0
Gus-Victrix <iamfrsncis@gmail.com>
2023-11-03 02:16:37 +0300

    Update to task 22

--- 103-binary_tree_rotate_left.c
+++ 103-binary_tree_rotate_left.c
@@ -9,18 +9,14 @@
  */
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
 {
-	binary_tree_t *temp = NULL;
+	binary_tree_t *pivot = NULL;
 
 	if (!tree || !tree->right)
 		return (NULL);
-	temp = tree->right;
-	tree->right = temp->left;
-	if (temp->parent)
-	{
-		if (temp->parent->left == tree)
-			temp->parent->left = temp;
-		else
-			temp->parent->right = temp;
-	}
-	return (temp);
+	pivot = tree->right;
+	tree->right = pivot->left;
+
+	pivot->parent = tree->parent;
+	tree->parent = pivot;
+	return (pivot);
 }

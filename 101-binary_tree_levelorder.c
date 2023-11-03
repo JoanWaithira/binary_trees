356d547071209f86150c45036032ae20e1514b38
JoanWaithira <joanwaithira.jw@gmail.com>
2023-11-03 02:40:46 -0700

    Level-order-traversal

--- 101-binary_tree_levelorder.c
+++ 101-binary_tree_levelorder.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_levelorder - A function that goes through a binary tree using level order traversal
+ * binary_tree_levelorder - A function that goes through a binary tree
  * @tree: A pointer to the root node of the tree to taverse
  * @func: A pointer to a function to call for each node
  */
@@ -9,7 +9,7 @@ void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (tree == NULL || func == NULL)
 		return;
-	binary_tree_t * queue[1024];
+	binary_tree_t *queue[1024];
 	int front = 0, rear = 0;
 
 	queue[rear++] = (binary_tree_t *)tree;
@@ -17,6 +17,7 @@ void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
 	while (front < rear)
 	{
 		const binary_tree_t *current = queue[front++];
+
 		func(current->n);
 
 		if (current->left)

39d5a0ba3e6df5029a4637c047eb957ae6aa4103
Gus-Victrix <iamfrsncis@gmail.com>
2023-11-04 01:01:52 +0300

    Added some more prototypes into the header file.

--- binary_trees.h
+++ binary_trees.h
@@ -29,7 +29,7 @@ typedef struct binary_tree_s heap_t;
 
 void binary_tree_print(const binary_tree_t *);
 
-/* Prototypes for Mandatory Tasks */
+/* Prototypes for binary trees*/
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
@@ -54,12 +54,27 @@ binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 int binary_tree_is_complete(const binary_tree_t *tree);
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
 binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree);
+void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
+
+/* Prototypes for binary search trees*/
 int binary_tree_is_bst(const binary_tree_t *tree);
 bst_t *bst_insert(bst_t **tree, int value);
 bst_t *array_to_bst(int *array, size_t size);
 bst_t *bst_search(const bst_t *tree, int value);
 bst_t *bst_remove(bst_t *root, int value);
+
+/* Prototypes for AVL trees*/
 int binary_tree_is_avl(const binary_tree_t *tree);
-void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
+avl_t *avl_insert(avl_t **tree, int value);
+avl_t *array_to_avl(int *array, size_t size);
+avl_t *avl_remove(avl_t *root, int value);
+avl_t *sorted_array_to_avl(int *array, size_t size);
+
+/* Prototypes for max binary heaps*/
+int binary_tree_is_heap(const binary_tree_t *tree);
+heap_t *heap_insert(heap_t **root, int value);
+heap_t *array_to_heap(int *array, size_t size);
+int heap_extract(heap_t **root);
+int *heap_to_sorted_array(heap_t *heap, size_t *size);
 
 #endif /* _BINARY_TREES_H_ */
